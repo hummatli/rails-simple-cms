ad5d8bf5885db3671435327beaebcbf2d4e47f8c
Sattar Hummatli <settarxan@gmail.com>
2018-01-08 19:35:44 +0400

    add controller, view and corresponding routes for them

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,12 @@
+class TestController < ApplicationController
+  def index
+  end
+
+  def action2
+
+  end
+
+  def action3
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'test/index'
+  get 'test/action2'
+  match 'test/22', :to => 'test#action3',
+    :via => :get
+
+  get 'example/index'
+
+  root 'demo#index'
+
+  get 'demo/index'
+
+  #default route
+  # get ':controller(/:action(/:id))'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
