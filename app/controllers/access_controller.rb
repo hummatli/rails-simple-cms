0d893a6a2b175b35b671fb3e4f72c904d6d0cb4b
Sattar Hummatli <settarxan@gmail.com>
2018-01-16 10:14:23 +0400

    add login and other controllers and views

---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,21 @@
+class AccessController < ApplicationController
+
+  layout 'admin'
+
+  def menu
+    #display text and links
+  end
+
+  def login
+    # login form
+  end
+
+  def attemt_login
+
+  end
+
+  def logout
+
+  end
+
+end
